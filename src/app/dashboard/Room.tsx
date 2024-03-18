cebbea4b9cbbb828fc552dee58ffebbadaf39b35
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-03-18 15:11:54 -0300

    feat: change room public

--- src/app/dashboard/Room.tsx
+++ src/app/dashboard/Room.tsx
@@ -5,23 +5,10 @@ import { RoomProvider } from "../../../liveblocks.config"
 import { ClientSideSuspense } from "@liveblocks/react"
 import Loader from "@/components/loader/Loader"
 import { LiveMap } from "@liveblocks/client"
-import { v4 } from "uuid"
 import { useSearchParams } from "next/navigation"
-import usePermission from "@/hooks/usePermission"
 
 export function Room({ children }: { children: ReactNode }) {
   const searchParams = useSearchParams()
-  const [isSharing, setIsSharing] = useState(false)
-
-  useEffect(() => {
-    const values = localStorage.getItem("room-id")!
-    const isSharing = JSON.parse(values)
-    setIsSharing(isSharing.state.isSharing)
-  }, [localStorage.getItem("room-id")])
-
-  useEffect(() => {
-    console.log("isSharing", isSharing)
-  }, [isSharing])
 
   return (
     <RoomProvider
